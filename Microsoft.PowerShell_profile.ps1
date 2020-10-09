0af2884d0e6ebf029ae42e418c0d3742563a0d47
cdom <dominguerilla@gmail.com>
2020-10-09 16:30:57 -0400

    Add prompt function

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,5 +7,9 @@ New-Alias -Name vim -Value 'C:\Program Files (x86)\vim\vim80\vim.exe'
 New-Alias -Name make-unity-repo -Value "$toolbox\make-unity-repo.ps1"
 New-Alias -Name grind -Value "$toolbox\grind.ps1"
 
+function prompt {
+	"$(Get-Location)> ";
+}
+
 # Include this if you like a vim command line experience
 Set-PSReadlineOption -EditMode vi -BellStyle Visual
